e91aacdeffa8b0bb26b4cc4151609dd51d0c8231
Sergey Lunev <s.lunev@gmail.com>
2020-11-18 18:10:50 +0300

    Http example updated

--- examples/http/index.js
+++ examples/http/index.js
@@ -6,12 +6,12 @@ console.log(`server is built in ${Date.now() - ts} msec`);
 server.use((ctx, next) => {
   //console.log('request:', ctx.request);
   let respText = 'HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n';
-  ctx.socket.write(respText);
+  ctx.connection.socket.write(respText);
 })
-.on('error', (evt) => {
-  console.error('Ошибка:', evt.error);
+.handler('errorConnection', (conn, err) => {
+  console.error('Ошибка:', err.toString());
   let respText = 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n';
-  evt.socket.write(respText);
+  conn.socket.write(respText);
 })
 .listen({
   port: 5555
